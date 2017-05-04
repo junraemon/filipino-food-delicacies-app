961130456345c9be2bc6c7ab9ef6bb0b39064552
Junrae Mon <junraemon@gmail.com>
2017-05-04 15:49:26 +0800

    update nativescript modules

--- app/main.aot.ts
+++ app/main.aot.ts
@@ -1,7 +1,14 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
+import { enableProdMode } from '@angular/core';
 import { platformNativeScript } from "nativescript-angular/platform-static";
 import firebase = require("nativescript-plugin-firebase");
 import { AppModuleNgFactory } from "./app.module.ngfactory";
 
-firebase.init({ persist: true }).then(() => console.log("firebase.init done"), (error) => console.log("firebase.init error: " + error));
+enableProdMode();
+firebase.init({ persist: true }).then(() => {
+   console.log("firebase.init done");
+   firebase.keepInSync("/", true)
+      .then(() => console.log("firebase.keepInSync is ON for /"),
+      (error) => console.log("firebase.keepInSync error: " + error));
+}, (error) => console.log("firebase.init error: " + error));
 platformNativeScript().bootstrapModuleFactory(AppModuleNgFactory);
--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,14 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
+import { enableProdMode } from '@angular/core';
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 import firebase = require("nativescript-plugin-firebase");
 import { AppModule } from "./app.module";
 
-firebase.init({ persist: true }).then(() => console.log("firebase.init done"), (error) => console.log("firebase.init error: " + error));
+enableProdMode();
+firebase.init({ persist: true }).then(() => {
+   console.log("firebase.init done");
+   firebase.keepInSync("/", true)
+      .then(() => console.log("firebase.keepInSync is ON for /"),
+      (error) => console.log("firebase.keepInSync error: " + error));
+}, (error) => console.log("firebase.init error: " + error));
 platformNativeScriptDynamic().bootstrapModule(AppModule);
